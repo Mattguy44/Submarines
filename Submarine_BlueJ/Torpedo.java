1ba654910c8386628d3153cf23c6a9156879816c
Mattguy44 <Mattjwr@gmail.com>
2015-05-14 14:54:21 -0500

    glowing-ironman-project to Submarine_BlueJ and projectiles in one Torpedo class

---
+++ Submarine_BlueJ/Torpedo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Torpedo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Torpedo
+{
+    int speed;
+    boolean isUnderwater;
+    /**
+     * Constructor for objects of class Torpedo
+     */
+    public Torpedo(int s, boolean uw)
+    {
+        speed = s;
+        isUnderwater = uw;
+    }
+
+}
