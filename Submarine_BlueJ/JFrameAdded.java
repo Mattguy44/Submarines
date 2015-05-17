23e7049c172111113f3015daf054d527fea7cde3
Orkhon Otgonbaatar <OrkhonO@orkhons-mbp.attlocal.net>
2015-05-16 23:04:21 -0500

    I added a useless JFrame that really doesn't do anything, but at least it's here

---
+++ Submarine_BlueJ/JFrameAdded.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class JFrameAdded here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.*;
+public class JFrameAdded extends JFrame
+{
+    /**
+     * Constructor for objects of class JFrameAdded
+     */
+    public JFrameAdded()
+    {
+        this.setSize(200,100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Submarine Game");
+        this.setVisible(true);
+    }
+    
+}
--- Submarine_BlueJ/Submarine.java
+++ Submarine_BlueJ/Submarine.java
@@ -5,21 +5,14 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-public abstract class Submarine extends JFrame
+import javax.swing.*;
+public abstract class Submarine extends JFrameAdded
 {
     private boolean isUnderwater = true;
     public Submarine(boolean isUnder){
         isUnderwater=isUnder;
-        this.setSize(200,100);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Submarine Game");
-        this.setVisible(true);
     }
     public static void main(String [] args){
-        new Submarine(boolean isUnder);
+        new JFrameAdded();
     }
 }
