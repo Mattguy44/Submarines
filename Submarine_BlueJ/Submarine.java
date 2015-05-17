9fc985707da9f475d39910d3b38a3bbe05673c7b
Orkhon Otgonbaatar <OrkhonO@orkhons-mbp.attlocal.net>
2015-05-16 22:47:46 -0500

    just tried to add a JFrame but failed, going to see what happened later

--- Submarine_BlueJ/Submarine.java
+++ Submarine_BlueJ/Submarine.java
@@ -5,7 +5,21 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Submarine
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+public abstract class Submarine extends JFrame
 {
-    private boolean isUnderwater;
+    private boolean isUnderwater = true;
+    public Submarine(boolean isUnder){
+        isUnderwater=isUnder;
+        this.setSize(200,100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Submarine Game");
+        this.setVisible(true);
+    }
+    public static void main(String [] args){
+        new Submarine(boolean isUnder);
+    }
 }
