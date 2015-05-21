a8885bf5f403a73622576c8e1db737e56a1e7b23
Neo1216 <twu6@students.d125.org>
2015-05-21 15:27:05 -0500

    delete method

--- Submarine_BlueJ/Torpedo.java
+++ Submarine_BlueJ/Torpedo.java
@@ -26,46 +26,61 @@ public class Torpedo
         speed = s;
         isUnderwater = uw;
     }
-    
-    
-    
-    public void update(Submarine opp){ //if the torpedo is firing, react correspondingly
-            if(obstacle[centerX][centerY]){ 
-                Torpedo this=null;
-            }
-            /*else if(Math.abs(opp.getCenterX-centerX)<= opp.getWidth()/2.
-            &&      Math.abs(opp.getCenterX-centerY)<= opp.getHeight()/2.){
-                opp.setExploding (true);
-                opp.setFrameNumber (1);        
-                Torpedo=null;
-            }*/
-            else{
-                if(direction.equals ("l") || direction.equals("L")
-                || direction.equals ("left") || direction.equals("LEFT"))
-                centerX-=speed;
-                else if(direction.equals("r") || direction.equals("R")
-                || direction.equals("right") || direction.equals("RIGHT"))
-                centerX+=speed; 
-                else if(direction.equals("u") || direction.equals("U")
-                || direction.equals("up") || direction.equals("UP"))
-                centerY-=speed;
-                else if(direction.equals("d") || direction.equals("D")
-                || direction.equals("down") || direction.equals("DOWN"))
-                centerY+=speed;
-                else
-                System.out.print("Error: check if right direction is assigned!");
-            }
-            //Note: the 4 choices in every if statements should be restricted to one, I add them because I would 
-            //like to know what's your guys' prefered parameter as direction
+
+    public void hit(Graphics g){
+        g.setColor(Color.yellow);
+
+        g.fillOval(centerX-4*explosionFrameNumber,
+            centerY-4*explosionFrameNumber,
+            8*explosionFrameNumber,
+            8*explosionFrameNumber);
+
+        g.setColor(Color.red);
+
+        g.fillOval(centerX-2*explosionFrameNumber,
+            centerY-explosionFrameNumber,
+            4*explosionFrameNumber,
+            2*explosionFrameNumber
+        );
     }
+
+    public void update(Submarine opp){ //if the torpedo is firing, react correspondingly
+        if(obstacle[centerX][centerY]){ 
+
+        }
+        /*else if(Math.abs(opp.getCenterX-centerX)<= opp.getWidth()/2.
+        &&      Math.abs(opp.getCenterX-centerY)<= opp.getHeight()/2.){
+        opp.setExploding (true);
+        opp.setFrameNumber (1);        
+        Torpedo=null;
+        }*/
+        else{
+            if(direction.equals ("l") || direction.equals("L")
+            || direction.equals ("left") || direction.equals("LEFT"))
+                centerX-=speed;
+            else if(direction.equals("r") || direction.equals("R")
+            || direction.equals("right") || direction.equals("RIGHT"))
+                centerX+=speed; 
+            else if(direction.equals("u") || direction.equals("U")
+            || direction.equals("up") || direction.equals("UP"))
+                centerY-=speed;
+            else if(direction.equals("d") || direction.equals("D")
+            || direction.equals("down") || direction.equals("DOWN"))
+                centerY+=speed;
+            else
+                System.out.print("Error: check if right direction is assigned!");
+        }
+        //Note: the 4 choices in every if statements should be restricted to one, I add them because I would 
+        //like to know what's your guys' prefered parameter as direction
+    }
+
     public void draw(Graphics g){ //draw the Torpedo
 
-        
         g.setColor(new Color(155,0,0));
         g.fillOval(centerX-tWidth/2,centerY-tHeight/2,tWidth,tHeight);
         //Make the Torpedo a circle for convenience.
     }
-    
+
     /*Advices for Submarine class:
      *0. Don't make it abstract and create subclassese like WASD and ARROWS as subclass
      *   They can all be done in KeyListener
